9eda78e1164a60260fc8757ee33d3045f6533f71
Fatima Amir Fahim <fahimfatimaamir@gmail.com>
2026-02-07 23:17:12 +0000

    Text Processing

--- chatbot.py
+++ chatbot.py
@@ -12,3 +12,12 @@ nltk.download('wordnet') #Using the WordNet Dictionay
 sent_tokens = nltk.sent_tokenize(raw_doc) #Converts doc to list of sentences
 word_tokens = nltk.word_tokenize(raw_doc) #Converts doc to list of words
 
+lemmer = nltk.stem.WordNetLemmatizer()
+#WordNet is a semantically-oriented dictionary of English included in NLTK
+def LemToken(token):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation) 
+# it's creating a translation dictionary that maps punctuation characters to None (which means "delete them")
+def LemNormalize(text):
+    return LemToken(nltk.word_tokenize(text.lower.translate(remove_punct_dict)))
+    #translate() is a string method that uses the dictionary you created to transform the text, in this case, to remove punctuation.
